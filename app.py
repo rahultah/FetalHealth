430cddf6a1261bf6c1daf2cc30c95ca547767d10
Rahul <rahul@Rahuls-MacBook-Pro.local>
2021-05-08 23:48:15 +0530

    Scaling commit

--- app.py
+++ app.py
@@ -1,9 +1,16 @@
 import numpy as np
+import pandas as pd 
 from flask import Flask, request, jsonify, render_template
 import pickle
+from sklearn.preprocessing import StandardScaler
+
+from sklearn import preprocessing
+
 
 app = Flask(__name__)
 model = pickle.load(open('model.pkl', 'rb'))
+s_scaler = preprocessing.StandardScaler()
+
 
 @app.route('/')
 def home():
@@ -14,9 +21,20 @@ def predict():
     '''
     For rendering results on HTML GUI
     '''
-    int_features = [int(x) for x in request.form.values()]
+    int_features = [float(x) for x in request.form.values()]
     final_features = [np.array(int_features)]
-    prediction = model.predict(final_features)
+
+    stan_data = pd.read_csv('stan.csv')
+    stan_data=stan_data.drop(["fetal_health"],axis=1)
+    stan_arr = np.array(stan_data)
+    stan_arr = np.append(stan_arr, np.array(final_features), axis=0)
+    stan_arr= s_scaler.fit_transform(stan_arr)
+    main_pred = stan_arr[2126]
+    # main_pred
+    prediction = model.predict([main_pred])
+    print(prediction)
+
+    # prediction = model.predict(final_features)
 
     output = prediction[0]
 
